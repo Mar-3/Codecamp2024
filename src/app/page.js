cddf3daef602b40b58c1b4063ac354c1b45802b9
Joona Kostamo <joona.kostamo.software@gmail.com>
2024-01-10 20:17:13 +0200

    Add Notice view to page

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import styles from './page.module.css'
 import { BaseView } from './Views/BaseView/BaseView';
 import { View2 } from './Views/View2/BaseView';
+import { Notice } from './Views/Notice/Notice';
 
 export default function App() {
   const [view, setView] = useState("base");
@@ -11,10 +12,12 @@ export default function App() {
       <div>
         <button onClick={() => {setView("base")}}>BaseView</button>
         <button onClick={() => {setView("view2")}}>view2</button>
+        <button onClick={() => {setView("notice")}}>notice</button>
       </div>
       <div>
         {view === "base" && <BaseView/>}
         {view === "view2" && <View2/>}
+        {view === "notice" && <Notice/>}
       </div>
     </main>
   )
