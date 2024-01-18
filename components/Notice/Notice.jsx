a43e05567f14bfc02e1691319a74f2b93e140b38
Joona Kostamo <joona.kostamo.software@gmail.com>
2024-01-18 20:01:39 +0200

    Use only MUI components in Notice, fix image

--- components/Notice/Notice.jsx
+++ components/Notice/Notice.jsx
@@ -1,30 +1,29 @@
 "use client";
-import React, {useState} from "react";
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
+import React, { useState } from "react";
 import PlaceIcon from "@mui/icons-material/Place";
 import MessageIcon from "@mui/icons-material/Message";
 import PersonIcon from "@mui/icons-material/Person";
-import { Modal, Paper } from "@mui/material";
+import { Modal, Paper, Grid, Box, Typography, Button } from "@mui/material";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import Image from "next/image";
+import { VerticalAlignTop } from "@mui/icons-material";
 
 export const Notice = ({ props }) => {
-
   const router = useRouter();
   const path = usePathname();
   const searchParams = useSearchParams();
 
   const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: '80%',
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: "80%",
+    bgcolor: "background.paper",
+    border: "2px solid #000",
     boxShadow: 24,
     p: 4,
-    padding: '3rem',
+    padding: "3rem",
   };
 
   var distance = 800; // In meters
@@ -44,9 +43,9 @@ export const Notice = ({ props }) => {
   const [error, setError] = useState(false);
 
   const toggleNotice = () => {
-      const params = new URLSearchParams(searchParams);
-      params.delete("id");
-      router.push(path + '?' + params.toString());
+    const params = new URLSearchParams(searchParams);
+    params.delete("id");
+    router.push(path + "?" + params.toString());
   };
 
   const handleImageError = () => {
@@ -55,64 +54,82 @@ export const Notice = ({ props }) => {
 
   return (
     <Modal open={true} onClose={toggleNotice}>
-        <Paper style={style} className="notice" aligncontent="space-between">
-          <Box className="notice-content">
-            <Box>
-              <Grid container>
-                <Grid item xs={8}>
-                  <p>{type}</p>
-                  <h2>{title}</h2>
-                  <Grid container className="notice-author-info">
-                    <Grid item xs={0.5} className="iconColumn">
-                      <PersonIcon className="icon" />
-                    </Grid>
-                    <Grid item xs={7.5}>
-                      <p className="author-profile-btn">
-                        Author:&nbsp; {" "}
-                        <button
-                          onClick={toggleNotice}
-                        >
-                          {author}
-                        </button>
-                      </p>
-                    </Grid>
+      <Paper style={style} className="notice" justifyContent="space-between">
+        <Box className="notice-content">
+          <Box>
+            <Grid container>
+              <Grid item xs={8}>
+                <Typography variant="p">{type}</Typography>
+                <Typography variant="h4">{title}</Typography>
+                <Grid container className="notice-author-info">
+                  <Grid item xs={0.5} className="iconColumn">
+                    <PersonIcon className="icon" />
                   </Grid>
-                  <Grid container className="notice-author-info">
-                    <Grid item xs={0.5} className="iconColumn">
-                      <PlaceIcon className="icon" />
-                    </Grid>
-                    <Grid item xs={7.5}>
-                      <p>In {area}</p>
-                      <p>
-                        ~{distance} {unit} from your location
-                      </p>
-                    </Grid>
-                  </Grid>
-                  <Grid container className="notice-author-info">
-                    <Grid item xs={0.5} className="iconColumn">
-                      <MessageIcon className="icon" />
-                    </Grid>
-                    <Grid item xs={7.5}>
-                      <button className="contact-btn" onClick={toggleNotice}>
-                        Contact {author}
-                      </button>
-                    </Grid>
+                  <Grid item xs={7.5}>
+                    <Typography variant="p">
+                      Author:&nbsp;{" "}
+                      <Button onClick={toggleNotice}>{author}</Button>
+                    </Typography>
                   </Grid>
                 </Grid>
-                <Grid item xs={4}>
-                  <Box
-                    display="flex"
-                    justifyContent="flex-end"
-                    className="profile-pic"
-                  >
-                  </Box>
+                <Grid container className="notice-author-info">
+                  <Grid item xs={0.5} className="iconColumn">
+                    <PlaceIcon className="icon" />
+                  </Grid>
+                  <Grid item xs={7.5}>
+                    <Typography variant="p" display="block">
+                      In {area}
+                    </Typography>
+                    <Typography variant="p">
+                      ~{distance} {unit} from your location
+                    </Typography>
+                  </Grid>
+                </Grid>
+                <Grid container className="notice-author-info">
+                  <Grid item xs={0.5} className="iconColumn">
+                    <MessageIcon className="icon" />
+                  </Grid>
+                  <Grid item xs={7.5}>
+                    <Button className="contact-btn" onClick={toggleNotice}>
+                      Contact {author}
+                    </Button>
+                  </Grid>
                 </Grid>
               </Grid>
-            </Box>
-            <br></br>
-            <p>{description}</p>
+              <Grid item xs={3}>
+                <Box
+                  display="flex"
+                  justifyContent="flex-end"
+                  className="profile-pic"
+                >
+                  {error ? (
+                    <Image
+                      src="/profile_picture.png"
+                      width={160}
+                      height={160}
+                      alt="User's profile picture"
+                    />
+                  ) : (
+                    <Image
+                      src={image}
+                      width={160}
+                      height={160}
+                      alt="User's profile picture"
+                      onError={handleImageError}
+                    />
+                  )}
+                </Box>
+              </Grid>
+              <Grid container justifyContent="flex-end" alignItems="flex-start" xs={1}>
+                <Button onClick={toggleNotice}>
+                  Close
+                </Button>
+              </Grid>
+            </Grid>
           </Box>
-        </Paper>
+          <Typography variant="p">{description}</Typography>
+        </Box>
+      </Paper>
     </Modal>
   );
 };
