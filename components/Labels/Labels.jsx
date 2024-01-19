0c13c81956e3d7e9cbe2c5679e8d9130c3cc1f80
Joona Kostamo <joona.kostamo.software@gmail.com>
2024-01-19 14:34:37 +0200

    Use separate labels element to create and render filters

---
+++ components/Labels/Labels.jsx
@@ -0,0 +1,61 @@
+"use client";
+import { React } from "react";
+import "../../src/app/globals.css";
+import {
+  Grid,
+  Checkbox,
+  FormGroup,
+  FormControlLabel,
+  FormLabel,
+  FormControl
+} from "@mui/material";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
+export const Labels = ({ props }) => {
+  const router = useRouter();
+  const path = usePathname();
+  const searchParams = useSearchParams();
+
+  const labels = props.labels;
+  const title = props.title
+
+  const noTypeSelected =
+    Object.keys(labels).filter((v) => isChecked(v)).length === 0;
+
+  let setToggle = (name, state) => {
+    const params = new URLSearchParams(searchParams);
+    params.set(name, state);
+    router.push(path + "?" + params.toString());
+  };
+
+  function isChecked(name) {
+    return !(searchParams.get(name) === "false");
+  }
+
+  return (
+    <Grid container className="button-column" alignItems="center" item xs={1.2}>
+      <FormControl
+        required
+        error={noTypeSelected}
+        component="fieldset"
+        sx={{ m: 3 }}
+        variant="standard"
+      >
+        <FormLabel component="legend">{title}</FormLabel>
+        <FormGroup>
+          {
+            Object.entries(labels).map(([urlLabel, shownLabel], i) => (
+              <FormControlLabel
+                control={<Checkbox checked={isChecked(String(urlLabel))} />}
+                label={shownLabel}
+                onChange={() => {
+                  setToggle(urlLabel, !isChecked(urlLabel));
+                }}
+              />
+            ))
+          }
+        </FormGroup>
+      </FormControl>
+    </Grid>
+  );
+};
--- components/Search/Search.jsx
+++ components/Search/Search.jsx
@@ -1,22 +1,25 @@
 "use client";
-import { React, useState } from "react";
+import { React } from "react";
 import TextField from "@mui/material/TextField";
 import "../../src/app/globals.css";
-import { Box, Grid, Typography, ToggleButton } from "@mui/material";
+import { Labels } from "@components/Labels/Labels";
+import {
+  Box,
+  Grid,
+  Typography
+} from "@mui/material";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 export const Search = ({ props }) => {
   const router = useRouter();
   const path = usePathname();
 
-  const [inputText, setInputText] = useState("");
+  const noticeTypes = {"lookingFor": "Looking for", "offering": "Offering", "selling": "Selling", "buying": "Buying", "attention": "Attention"}
 
   const searchParams = useSearchParams();
   const search = searchParams.get("search") ?? "";
-  var lookingFor = !(searchParams.get("lookingFor") === "false");
-  var offering = !(searchParams.get("offering") === "false");
-  var input = "";
 
+  var input = "";
   let inputHandler = (e) => {
     // //convert input text to lower case
     input = e.target.value.toLowerCase();
@@ -25,17 +28,11 @@ export const Search = ({ props }) => {
     router.push(path + "?" + params.toString());
   };
 
-  let setToggle = (name, state) => {
-    const params = new URLSearchParams(searchParams);
-    params.set(name, state);
-    router.push(path + "?" + params.toString());
-  };
-
   return (
     <Box className="main" marginTop={2} marginBottom={2} marginLeft={3 + "em"}>
       <Grid container>
-        <Grid item xs={6} marginRight={2}>
-          <Box className="search">
+        <Grid container item xs={6} marginRight={2} alignItems="center">
+          <Box className="search" width={100 + "%"}>
             <TextField
               id="search-input"
               onChange={inputHandler}
@@ -46,35 +43,18 @@ export const Search = ({ props }) => {
             />
           </Box>
         </Grid>
-        <Grid container className="filter-text-column" alignItems="center" item xs={0.8}>
+        <Grid
+          container
+          className="filter-text-column"
+          alignItems="center"
+          item
+          xs={0.8}
+        >
           <Typography variant="p" className="filter-text">
             Searching for:
           </Typography>
         </Grid>
-        <Grid container className="button-column" alignItems="center" item xs={1}>
-          <ToggleButton
-            id="looking-for-button"
-            value={lookingFor}
-            className={lookingFor ? "toggle-button-green" : "toggle-button-red"}
-            onChange={() => {
-              setToggle("lookingFor", !lookingFor);
-            }}
-          >
-            Looking For
-          </ToggleButton>
-        </Grid>
-        <Grid container className="button-column" alignItems="center" item xs={1}>
-          <ToggleButton
-            id="offering-button"
-            value={offering}
-            className={offering ? "toggle-button-green" : "toggle-button-red"}
-            onChange={() => {
-              setToggle("offering", !offering);
-            }}
-          >
-            Offering
-          </ToggleButton>
-        </Grid>
+        <Labels props={{"title": "Notice type", "labels": noticeTypes}}></Labels>
       </Grid>
     </Box>
   );
